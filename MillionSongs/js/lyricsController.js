1446a3b7538730825ae96416b7c3d789f050317a
FailX <crank_felix@gmx.de>
2016-05-26 01:25:52 +0200

    changed the way the fontsize of the bag of words is determined

--- MillionSongs/js/lyricsController.js
+++ MillionSongs/js/lyricsController.js
@@ -20,103 +20,157 @@ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 
-var lyricsUpdate = function(){
+var lyricsUpdate = function () {
 
-    d3.csv("data/lyrics/" + genre1Selected +"/"+decadeForLyricsTOP+".csv", function(error, data){
-    
-    //Empty the clouds previously made. Transition someday...
-    $("#genre1cloud").empty();
-    
-    var wordsTop = [];
-    
+    d3.csv("data/lyrics/" + genre1Selected + "/" + decadeForLyricsTOP + ".csv", function (error, data) {
+
+        //Empty the clouds previously made. Transition someday...
+        $("#genre1cloud").empty();
+
+        var wordsTop = [];
 
 
-    for (i = 0; i < data.length; i++){
-        if(data[i].count> 8){
-            wordsTop.push({"text":data[i].word, "size":data[i].count});
-               //push the words in the right number of times
+
+        for (i = 0; i < data.length; i++) {
+            if (data[i].count > 8) {
+                wordsTop.push({
+                    "text": data[i].word,
+                    "size": data[i].count
+                });
+                //push the words in the right number of times
+            }
+
+        };
+        //get max word count
+        if (typeof wordsTop[0] != "undefined") {
+
+            var max = wordsTop[0].size;
+            var x = 1;
+            if (max < 60) {
+                x = 2;
+            } else if (max > 130) {
+                x = 0.5;
+            }
         }
-            
-    };
- 
+
         var fill = d3.scale.category20();
         d3.layout.cloud().size([350, 500])
-            .words(wordsTop.map(function(d) {
-                return {text: d.text, size: d.size};
-        }))
-        .padding(5)
-        .rotate(function() { return ~~(Math.random() * 1) * 90; })
-        .font("Impact")
-        .fontSize(function(d) { return ((d.size) * 2); })
-        .on("end", draw)
-        .start();
+            .words(wordsTop.map(function (d) {
+                return {
+                    text: d.text,
+                    size: d.size
+                };
+            }))
+            .padding(5)
+            .rotate(function () {
+                return ~~(Math.random() * 1) * 90;
+            })
+            .font("Impact")
+            .fontSize(function (d) {
+                return ((d.size) * x);
+            })
+            .on("end", draw)
+            .start();
+
         function draw(words) {
             d3.select("#genre1cloud").append("svg")
-            .attr("width", 350)
-            .attr("height", 500)
-            .append("g")
-            .attr("transform", "translate(150,250)")
-            .selectAll("text")
-            .data(words)
-            .enter().append("text")
-            .style("font-size", function(d) { return d.size + "px"; })
-            .style("font-family", "Impact")
-            .style("fill", function(d, i) { return fill(i); })
-            .attr("text-anchor", "middle")
-            .attr("transform", function(d) {
-                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-            })
-            .text(function(d) { return d.text; });
+                .attr("width", 350)
+                .attr("height", 500)
+                .append("g")
+                .attr("transform", "translate(170,260)")
+                .selectAll("text")
+                .data(words)
+                .enter().append("text")
+                .style("font-size", function (d) {
+                    return d.size + "px";
+                })
+                .style("font-family", "Impact")
+                .style("fill", function (d, i) {
+                    return fill(i);
+                })
+                .attr("text-anchor", "middle")
+                .attr("transform", function (d) {
+                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                })
+                .text(function (d) {
+                    return d.text;
+                });
         }
 
     });
-    
-    d3.csv("data/lyrics/" + genre2Selected +"/"+decadeForLyricsBOT+".csv", function(error, data){
-    
-        
-    //Empty the clouds previously made. Transition someday...
-    $("#genre2cloud").empty();
 
-    var wordsBottom = [];
-    
-     for (i = 0; i < data.length; i++){
-        if(data[i].count> 8){
-            wordsBottom.push({"text":data[i].word, "size":data[i].count});
-               //push the words in the right number of times
-           }
-            
-    };
- 
+    d3.csv("data/lyrics/" + genre2Selected + "/" + decadeForLyricsBOT + ".csv", function (error, data) {
+
+
+        //Empty the clouds previously made. Transition someday...
+        $("#genre2cloud").empty();
+
+        var wordsBottom = [];
+
+        for (i = 0; i < data.length; i++) {
+            if (data[i].count > 8) {
+                wordsBottom.push({
+                    "text": data[i].word,
+                    "size": data[i].count
+                });
+                //push the words in the right number of times
+            }
+
+        };
+        //get max word count
+        if (typeof wordsBottom[0] != "undefined") {
+
+            var max = wordsBottom[0].size;
+            var x = 1;
+            if (max < 60) {
+                x = 2;
+            } else if (max > 130) {
+                x = 0.5;
+            }
+        }
         var fill = d3.scale.category20();
-        
-        
+
         d3.layout.cloud().size([350, 500])
-            .words(wordsBottom.map(function(d) {
-                return {text: d.text, size: d.size};
-        }))
-        .padding(5)
-        .rotate(function() { return ~~(Math.random() * 1) * 90; })
-        .font("Impact")
-        .fontSize(function(d) { return ((d.size) * 2); })
-        .on("end", draw)
-        .start();
+            .words(wordsBottom.map(function (d) {
+                return {
+                    text: d.text,
+                    size: d.size
+                };
+            }))
+            .padding(5)
+            .rotate(function () {
+                return ~~(Math.random() * 1) * 90;
+            })
+            .font("Impact")
+            .fontSize(function (d) {
+                return ((d.size) * x);
+            })
+            .on("end", draw)
+            .start();
+
         function draw(words) {
             d3.select("#genre2cloud").append("svg")
-            .attr("width", 350)
-            .attr("height", 500)
-            .append("g")
-            .attr("transform", "translate(150,250)")
-            .selectAll("text")
-            .data(words)
-            .enter().append("text")
-            .style("font-size", function(d) { return d.size + "px"; })
-            .style("font-family", "Impact")
-            .style("fill", function(d, i) { return fill(i); })
-            .attr("text-anchor", "middle")
-            .attr("transform", function(d) {
-                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-            })
-            .text(function(d) { return d.text; });
+                .attr("width", 350)
+                .attr("height", 500)
+                .append("g")
+                .attr("transform", "translate(170,260)")
+                .selectAll("text")
+                .data(words)
+                .enter().append("text")
+                .style("font-size", function (d) {
+                    return d.size + "px";
+                })
+                .style("font-family", "Impact")
+                .style("fill", function (d, i) {
+                    return fill(i);
+                })
+                .attr("text-anchor", "middle")
+                .attr("transform", function (d) {
+                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                })
+                .text(function (d) {
+                    return d.text;
+                });
         }
 
 
